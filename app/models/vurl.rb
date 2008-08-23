e660e82d8271b9855829e7edeb89f3f88f278d1d
Veez (Matt Remsik) <mremsik@gmail.com>
2008-08-23 09:22:11 -0400

    Make slug autogenerate

--- app/models/vurl.rb
+++ app/models/vurl.rb
@@ -1,2 +1,11 @@
 class Vurl < ActiveRecord::Base
+  validates_presence_of :url
+
+  def before_save
+    if vurl = Vurl.find(:first, :order => 'slug DESC')
+      self.slug = vurl.slug.succ
+    else
+      self.slug = 'AAAA'
+    end
+  end
 end
