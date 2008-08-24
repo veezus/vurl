21c63cf6516734d9cb06b0e7c1cbc40d8d7ee44c
Veez (Matt Remsik) <mremsik@gmail.com>
2008-08-24 13:36:49 -0400

    Add specs for vurls

---
+++ spec/models/vurl_spec.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe "Vurl" do
+
+  it "should require a url" do
+    Factory.build(:vurl, :url => '').should_not be_valid
+    Factory.build(:vurl, :url => 'invalid_url').should_not be_valid
+    Factory.build(:vurl, :url => 'http://sub-domain.mattremsik.com').should be_valid
+  end
+end
