51271a3c7cf97a9ccc83ff5ced000e93840cdcd7
Veez (Matt Remsik) <mremsik@gmail.com>
2008-08-27 23:31:45 -0400

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,4 @@
+default_run_options[:pty] = true
 set :application, "vurl"
 set :repository,  "git@github.com:veez/vurl.git"
 
@@ -6,11 +7,13 @@ set :repository,  "git@github.com:veez/vurl.git"
 # via the :deploy_to variable:
 # set :deploy_to, "/var/www/#{application}"
 set :deploy_to, "~/vurl"
+set :deploy_via, :remote_cache
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
 set :scm, :git
+set :branch, 'master'
 
 role :app, "mattremsik.com"
 role :web, "mattremsik.com"
@@ -19,3 +22,8 @@ role :db,  "mattremsik.com", :primary => true
 set :user, "craftst"
 set :scm_username, "veez"
 set :use_sudo, false
+
+after "deploy:update_code", "vurl_custom"
+task :vurl_custom, :roles => :app, :except => {:no_release => true, :no_symlink => true} do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+end
