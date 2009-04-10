12c4f06518f79a4e6294ebd5c5985b40713de653
Veez (Matt Remsik) and Philip Crawford <dev+veez+wiscodude@hashrocket.com>
2009-04-10 12:44:08 -0400

    Use titles, keywords, and description from target url when available

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def page_title
+    @vurl && !@vurl.title.blank? ?  @vurl.title : "Veez's URL shortener"
+  end
 end
