3afa1cb621165df3210c8dd9209289a4ec629f5c
Veez (Matt Remsik) <mremsik@gmail.com>
2009-04-20 22:08:00 -0400

    Display most popular vurls on the home page

--- spec/views/vurls/new.html.haml_spec.rb
+++ spec/views/vurls/new.html.haml_spec.rb
@@ -3,9 +3,16 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper.rb')
 describe "new vurl page" do
   before do
     assigns[:vurl] = Vurl.new
+    Vurl.any_instance.stubs(:fetch_url_data).returns(true)
   end
   it "has an I'm Feeling Lucky link" do
     render "/vurls/new.html.haml"
     response.should have_tag("a[href=?]", random_vurls_path, "I'm Feeling Lucky")
+  end
+  it "has most popular vurls" do
+    vurl = Factory(:vurl, :title => 'title', :url => 'http://example.com')
+    Vurl.stub!(:most_popular).and_return([vurl])
+    render "/vurls/new.html.haml"
+    response.should have_tag("a[href=?]", vurl.url, vurl.title)
   end
 end
