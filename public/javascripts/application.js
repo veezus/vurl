a060cfdbc0ab7c4986f556c1d0aafc2f4beb96e7
Veez (Matt Remsik) <mremsik@gmail.com>
2009-05-21 19:37:11 -0400

    Add copy to clipboard funcationality

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,4 +4,13 @@ $(document).ready(function($) {
     fixed: false
   });
   $('#vurl_url').focus();
+  setup_clipboard();
 });
+
+function setup_clipboard() {
+  ZeroClipboard.setMoviePath('http://localhost:3000/ZeroClipboard.swf');
+  vurl_link = $('#vurlified_url');
+  clip = new ZeroClipboard.Client();
+  clip.glue('copy_to_clipboard');
+  clip.setText(vurl_link.attr('href'));
+}
