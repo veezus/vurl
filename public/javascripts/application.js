9e3f1602d63132473acb909be5dff49fece054cc
Big Tiger <bigtiger@hashrocket.com>
2009-05-31 17:31:25 -0400

    Bookmarklet instructions to tooltip, refactor js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,20 @@
-$(document).ready(function($) {
-  $('.lucky_link').simpletip({
-    content: "Click to view a random vurl from the archives!",
-    fixed: false
-  });
-  $('#vurl_url').focus();
+$(document).ready(function() {
+  setup_lucky_tooltip();
+  setup_bookmarklet_tooltip();
+  set_focus_on_url_input();
   setup_clipboard();
 });
+
+function set_focus_on_url_input() {
+  $('#vurl_url').focus();
+}
+
+function setup_bookmarklet_tooltip() {
+  $('.bookmarklet_help').simpletip({
+    content: "Drag the 'Vurlify!' link to your toolbar to vurlify any page address while you're browsing",
+    fixed: false
+  });
+}
 
 function setup_clipboard() {
   ZeroClipboard.setMoviePath('http://localhost:3000/ZeroClipboard.swf');
@@ -13,4 +22,11 @@ function setup_clipboard() {
   clip = new ZeroClipboard.Client();
   clip.glue('copy_to_clipboard');
   clip.setText(vurl_link.attr('href'));
+}
+
+function setup_lucky_tooltip() {
+  $('.lucky_link').simpletip({
+    content: "Click to view a random vurl from the archives!",
+    fixed: false
+  });
 }
