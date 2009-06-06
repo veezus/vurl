705f72240d258fc06e61f347add028d9ceb5008a
Veez (Matt Remsik) <mremsik@gmail.com>
2009-06-06 17:21:48 -0400

    Add mojombo's clippy for vurl copy to clipboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,33 @@ module ApplicationHelper
   def page_title
     @vurl && !@vurl.title.blank? ?  @vurl.title : "Veez's URL shortener"
   end
+
+  # Thanks to mojombo for his clippy swf
+  # http://github.com/mojombo/clippy
+  def clippy(text, bgcolor='#FFFFFF')
+    html = <<-EOF
+    <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
+            width="110"
+            height="14"
+            id="clippy" >
+    <param name="movie" value="/flash/clippy.swf"/>
+    <param name="allowScriptAccess" value="always" />
+    <param name="quality" value="high" />
+    <param name="scale" value="noscale" />
+    <param NAME="FlashVars" value="text=#{text}">
+    <param name="bgcolor" value="#{bgcolor}">
+    <embed src="/flash/clippy.swf"
+           width="110"
+           height="14"
+           name="clippy"
+           quality="high"
+           allowScriptAccess="always"
+           type="application/x-shockwave-flash"
+           pluginspage="http://www.macromedia.com/go/getflashplayer"
+           FlashVars="text=#{text}"
+           bgcolor="#{bgcolor}"
+    />
+    </object>
+    EOF
+  end
 end
