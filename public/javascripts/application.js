60a5b4d3887f4b20c1f9d1e977541b6e77ad02f6
Veez (Matt Remsik) <mremsik@gmail.com>
2009-06-06 17:35:34 -0400

    Remove ZeroClipboard

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   setup_lucky_tooltip();
   setup_bookmarklet_tooltip();
   set_focus_on_url_input();
-  setup_clipboard();
 });
 
 function set_focus_on_url_input() {
@@ -14,14 +13,6 @@ function setup_bookmarklet_tooltip() {
     content: "Drag the 'Vurlify!' link to your toolbar to vurlify any page address while you're browsing",
     fixed: false
   });
-}
-
-function setup_clipboard() {
-  ZeroClipboard.setMoviePath('http://localhost:3000/ZeroClipboard.swf');
-  vurl_link = $('#vurlified_url');
-  clip = new ZeroClipboard.Client();
-  clip.glue('copy_to_clipboard');
-  clip.setText(vurl_link.attr('href'));
 }
 
 function setup_lucky_tooltip() {
