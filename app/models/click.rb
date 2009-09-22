4eadf514fef15410feb726fc145c02a06bacb24a
Veezus Kreist <mremsik@gmail.com>
2009-09-21 20:33:21 -0400

    Click scopes by day

--- app/models/click.rb
+++ app/models/click.rb
@@ -1,4 +1,6 @@
 class Click < ActiveRecord::Base
   belongs_to :vurl, :counter_cache => true
   validates_presence_of :vurl_id, :ip_address, :user_agent
+
+  named_scope :by_day, lambda {|date| { :conditions => ["created_at >= ? AND created_at <= ?", date.midnight, (1.day.from_now(date.midnight) - 1.second)] } }
 end
