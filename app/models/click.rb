23863c929a5503718f965a8ea88a5a8d7cc9c1ef
Veezus Kreist <mremsik@gmail.com>
2009-09-28 22:38:01 -0400

    Dead code removal

--- app/models/click.rb
+++ app/models/click.rb
@@ -3,7 +3,4 @@ class Click < ActiveRecord::Base
   validates_presence_of :vurl_id, :ip_address, :user_agent
 
   named_scope :since, lambda {|date| { :conditions => ["created_at >= ?", date] } }
-  named_scope :by_day, lambda {|date| { :conditions => ["created_at >= ? AND created_at <= ?", date.midnight, (1.day.from_now(date.midnight) - 1.second)] } }
-  named_scope :by_hour, lambda {|date| { :conditions => ["created_at >= ? AND created_at <= ?", date.change(:hour => date.hour), date.change(:hour => date.hour + 1)] } }
-  named_scope :by_minute, lambda {|date| { :conditions => ["created_at >= ? AND created_at <= ?", date, date + 1.minute] } }
 end
