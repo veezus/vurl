9a935975fa1d4c1154597b35289485d0760a07a8
Veezus Kreist <mremsik@gmail.com>
2009-11-14 12:18:11 -0500

    Integration spec for unsuccessful vurl creation

--- spec/integrations/create_vurl_spec.rb
+++ spec/integrations/create_vurl_spec.rb
@@ -1,13 +1,24 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 
 describe "Create Vurls" do
-  it "redirects to the vurl stats page" do
-    submit_vurl 'http://google.com'
-    current_url.should == stats_url(Vurl.last.slug)
+  context "when successful" do
+    before { submit_vurl 'http://veez.us' }
+    it "redirects to the vurl stats page" do
+      current_url.should == stats_url(Vurl.last.slug)
+    end
+    it "creates a vurl" do
+      Vurl.last.url.should == 'http://veez.us'
+    end
   end
-  it "creates a vurl" do
-    submit_vurl 'http://veez.us'
-    Vurl.last.url.should == 'http://veez.us'
+
+  context "when unsuccessful" do
+    before { submit_vurl 'whatthe?' }
+    it "re-renders the new vurl page" do
+      response.should render_template('vurls/new')
+    end
+    it "doesn't create a vurl" do
+      Vurl.last.should be_nil
+    end
   end
 end
 
