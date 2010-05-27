0d9fe1971f8c04071b91eed4e35d738131cc1be0
Matt Yoho and Veezus Kreist <dev+mattyoho+veezus@hashrocket.com>
2010-05-27 15:59:02 -0500

    Tweak twitter configuration

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -13,13 +13,11 @@ function set_focus_on_url_input() {
 }
 
 function loadTweets() {
-  $("#vurlme_tweets").fadeOut();
   $("#vurlme_tweets").empty();
   $("#vurlme_tweets").tweet({
     query: "vurl.me OR vurlme",
     avatar_size: 48,
-    count: 15,
+    count: 10,
     loading_text: "Searching twitter..."
   });
-  $("#vurlme_tweets").fadeIn();
 }
