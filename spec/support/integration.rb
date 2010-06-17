cedf772ff06849a95f7268989057a141d580ac55
Veezus Kreist <mremsik@gmail.com>
2010-06-17 17:26:10 -0500

    Remove crufty matcher

--- spec/support/integration.rb
+++ spec/support/integration.rb
@@ -3,10 +3,3 @@ class ActiveSupport::TestCase
     URI.parse(current_url).path
   end
 end
-
-Spec::Matchers.define :contain_text do |text|
-  match do |response_body|
-    response_text = Webrat.nokogiri_document(response_body).text.squish
-    response_text.include?(text)
-  end
-end
