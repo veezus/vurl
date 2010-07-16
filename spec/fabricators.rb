277c7a11bc0de3542f56d3ba82828ebe61b060d5
Veezus Kreist <mremsik@gmail.com>
2010-07-16 12:59:09 -0500

    Vurl schematic assigns a user
    Re-order the fabricator file and add a user to the Vurl schematic. The
    user assignment was working correctly, so assign it after create and
    manually save the record.

--- spec/fabricators.rb
+++ spec/fabricators.rb
@@ -1,19 +1,23 @@
-Fabricator(:vurl) do
-  url 'http://mattremsik.com'
-  title 'Matt Remsik'
-  user { Fabricate(:user) }
-end
-
-Fabricator(:vurl_with_clicks, :from => :vurl) do
-
-  after_create do |v|
-    v.clicks << Fabricate(:click, :vurl => v)
-  end
-end
-
-Fabricator(:user) {}
-
 Fabricator(:click) do
   ip_address '127.0.0.1'
   user_agent '1337 browser'
+end
+
+Fabricator(:user) do
+  name 'Anonymous'
+end
+
+Fabricator(:vurl) do
+  after_create do |vurl|
+    vurl.user = Fabricate(:user)
+    vurl.save
+  end
+  url 'http://veez.us'
+  title 'Veezus Kreist'
+end
+
+Fabricator(:vurl_with_clicks, :from => :vurl) do
+  after_create do |v|
+    v.clicks << Fabricate(:click, :vurl => v)
+  end
 end
