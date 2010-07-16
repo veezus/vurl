2a5b1c301c77b5e161b78f2c60561fcb84786d80
Veezus Kreist <mremsik@gmail.com>
2010-07-16 13:03:38 -0500

    Display spam notice for flagged vurls

--- spec/integration/view_stats_spec.rb
+++ spec/integration/view_stats_spec.rb
@@ -8,6 +8,21 @@ describe "View stats" do
     page.should have_css("a[href='#{redirect_url(vurl.slug)}']", :text => redirect_url(vurl.slug))
   end
 
+  context "when the vurl has been marked as spam" do
+    before { vurl.flag_as_spam }
+    it "includes a notice" do
+      visit stats_path(vurl.slug)
+      page.should have_css("#spam_warning", :text => /flagged as spam/)
+    end
+  end
+
+  context "when the vurl is not spam" do
+    it "doesn't include a spam notice" do
+      visit stats_path(vurl.slug)
+      page.should_not have_css("#spam_warning", :text => /flagged as spam/)
+    end
+  end
+
   context "with clicks more than an hour old" do
     before do
       3.times { Fabricate(:click, :vurl => vurl) }
