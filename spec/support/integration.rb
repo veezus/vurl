8e66acecf2131df41c745af2a196fe46434cb858
Bernerd Schaefer and Veezus Kreist <dev+bernerdschaefer+veezus@hashrocket.com>
2010-07-30 15:03:25 -0500

    Cleanup

--- spec/support/integration.rb
+++ spec/support/integration.rb
@@ -3,12 +3,6 @@ class ActiveSupport::TestCase
     URI.parse(current_url).path
   end
 
-  def visit_as_user(path)
-    # request.cookies[:user_id] = user.id
-    p page.driver.set_cookie("user_id;#{user.id}", URI.parse(page.current_url))
-    visit path
-  end
-
   def login_as(user)
     visit root_path
     click_link 'log in as another user'
