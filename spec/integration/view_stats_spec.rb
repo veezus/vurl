e7567dad9a5115171b013f9bd5376319b8ece54e
Bernerd Schaefer and Veezus Kreist <dev+bernerdschaefer+veezus@hashrocket.com>
2010-08-09 11:07:06 -0500

    Display user information on vurl summary

--- spec/integration/view_stats_spec.rb
+++ spec/integration/view_stats_spec.rb
@@ -2,10 +2,58 @@ require 'spec_helper'
 
 describe "View stats" do
   let(:vurl) { Fabricate(:vurl) }
+  let(:user) { vurl.user }
 
   it "displays the new vurl" do
     visit stats_path(vurl.slug)
     page.should have_css("a[href='#{redirect_url(vurl.slug)}']", :text => redirect_url(vurl.slug))
+  end
+
+  context "when the user has profile information" do
+    executes do
+      user.update_attributes(:name => 'My Name', :blog => 'http://blog.oinopa.com', :website => 'http://oinopa.com')
+      visit stats_path(vurl.slug)
+    end
+    it "displays the user's blog" do
+      page.should have_css("a[href='http://blog.oinopa.com']")
+    end
+    it "displays the user's name" do
+      page.should have_content(user.name)
+    end
+    it "displays the user's website" do
+      page.should have_css("a[href='http://oinopa.com']")
+    end
+  end
+
+  context "when the user has no name" do
+    it "displays 'Anonymous'" do
+      visit stats_path(vurl.slug)
+      page.should have_content("By Anonymous")
+    end
+  end
+
+  context "when the user has a blog but no website" do
+    it "is grammatically correct" do
+      user.update_attribute(:blog, 'http://blog.oinopa.com')
+      visit stats_path(vurl.slug)
+      page.should have_content("(blog)")
+    end
+  end
+
+  context "when the user has no blog but does have a website" do
+    it "is grammatically correct" do
+      user.update_attribute(:website, 'http://oinopa.com')
+      visit stats_path(vurl.slug)
+      page.should have_content("(website)")
+    end
+  end
+
+  context "when the user has no blog and no website" do
+    it "doesn't reference either" do
+      visit stats_path(vurl.slug)
+      page.should_not have_content("blog")
+      page.should_not have_content("website")
+    end
   end
 
   context "when the vurl has been marked as spam" do
