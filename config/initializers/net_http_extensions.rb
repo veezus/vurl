7eaad5e9fe9a44aadd1789238e7908e04535d456
Attila Domokos and Veezus Kreist <almighty+ad+vk@veez.us>
2012-02-02 02:34:15 -0500

    Include SSL certificate bundle
    [#9462801]

---
+++ config/initializers/net_http_extensions.rb
@@ -0,0 +1,11 @@
+module Net
+  class HTTP
+    alias_method :original_use_ssl=, :use_ssl=
+
+    def use_ssl=(value)
+      self.ca_file = Rails.root.join('lib/ca-bundle.crt').to_s
+      self.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      self.original_use_ssl = value
+    end
+  end
+end
